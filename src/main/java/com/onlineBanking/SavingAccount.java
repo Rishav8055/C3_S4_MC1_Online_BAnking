d38df68a64fae29b69958a7125e0394ddd42ce84
rishavemail786 <rishavemail786@gmail.com>
2022-08-19 12:46:50 +0530

    SOLUTION DONE

--- src/main/java/com/onlineBanking/SavingAccount.java
+++ src/main/java/com/onlineBanking/SavingAccount.java
@@ -1,2 +1,53 @@
-package com.onlineBanking;public class SavingAccount {
+package com.onlineBanking;
+
+public class SavingAccount extends BankAccount{
+
+    private String holderName;
+    private String holderIdProof;
+    private String holderAddress;
+
+    public SavingAccount(int accountNumber, String accountOpeningDate, String modeOfOperation, boolean internetBanking, int availableBalance, String holderName, String holderIdProof, String holderAddress) {
+        super(accountNumber, accountOpeningDate, modeOfOperation, internetBanking, availableBalance);
+        this.holderName = holderName;
+        this.holderIdProof = holderIdProof;
+        this.holderAddress = holderAddress;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public String getHolderIdProof() {
+        return holderIdProof;
+    }
+
+    public void setHolderIdProof(String holderIdProof) {
+        this.holderIdProof = holderIdProof;
+    }
+
+    public String getHolderAddress() {
+        return holderAddress;
+    }
+
+    public void setHolderAddress(String holderAddress) {
+        this.holderAddress = holderAddress;
+    }
+    @Override
+    public int showAccountBalance(){
+        return getAvailableBalance();
+    }
+    @Override
+    public int creditBalance(int amount){
+        return getAvailableBalance();
+
+    }
+
+    @Override
+    public int debitBalance(int amount) {
+        return getAvailableBalance()-amount;
+    }
 }
--- src/main/java/com/onlineBanking/SavingAccountIMPL.java
+++ src/main/java/com/onlineBanking/SavingAccountIMPL.java
@@ -1,7 +1,20 @@
 package com.onlineBanking;
 
-public class BankAccountIMPL {
+public class SavingAccountIMPL {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SavingAccount savingAccount = new SavingAccount(12345678,"12-02-1234","self",true,5000,"risahv","123fgh","begusarai");
+        savingAccount.setAccountNumber(1236543256);
+        savingAccount.setHolderName("rishav");
+        savingAccount.setAccountOpeningDate("22-05-2015");
+        savingAccount.setHolderIdProof("1233455");
+        savingAccount.setAvailableBalance(5000);
+        savingAccount.setModeOfOperation("self");
+        savingAccount.setInternetBanking(true);
+
+        System.out.println(savingAccount.getINTEREST_RATE());
+        System.out.println(savingAccount.creditBalance(2000));
+        System.out.println(savingAccount.debitBalance(1000));
+        System.out.println(savingAccount.showAccountBalance());
+        }
+
     }
-}
